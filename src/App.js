6512a6d8affc5cfa1b0881b411c2042786e57899
Vishaal <vishaal.s2020@vitstudent.ac.in>
2022-10-27 12:03:58 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { useEffect } from "react";
 import HomePage from "./Views/HomePage";
-import { requestPermission} from "./init-fcm";
+import { requestPermission } from "./init-fcm";
 import { getMessaging, onMessage } from "firebase/messaging";
+import LoginPage from "./Views/LoginPage";
 function App() {
-    useEffect(() => {
-        const messaging = getMessaging();
-        onMessage(messaging, (payload) => {
-            console.log('Message received. ', payload);
-        });
-    }, [])
+    // useEffect(() => {
+    //     const messaging = getMessaging();
+    //     onMessage(messaging, (payload) => {
+    //         console.log('Message received. ', payload);
+    //     });
+    // }, [])
     return (
         <div className="App">
-            <HomePage />
-            <button onClick={requestPermission} >
+            <LoginPage />
+            {/* <button onClick={requestPermission} >
                 Click to receive notifications
-            </button>
+            </button> */}
         </div>
     );
 }
--- src/init-fcm.js
+++ src/init-fcm.js
@@ -1,12 +1,45 @@
 import { getMessaging, getToken } from "firebase/messaging";
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+const app = initializeApp({
+    apiKey: "AIzaSyC7-ZMp-ywmGmTMI1jjRrQDTZaspiI1Rrk",
+    authDomain: "farm-life-d1c77.firebaseapp.com",
+    projectId: "farm-life-d1c77",
+    storageBucket: "farm-life-d1c77.appspot.com",
+    messagingSenderId: "51901782398",
+    appId: "1:51901782398:web:4e476c39b6018404a7fb2f",
+    measurementId: "G-TP7ZGR6B26"
+})
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider()
+export const signInWithGoogle = (e) => {
+    e.preventDefault()
+    signInWithPopup(auth, provider)
+        .then((res) => {
+            localStorage.setItem('name', res.user.displayName)
+            localStorage.setItem('email', res.user.email)
+            localStorage.setItem('photoUrl', res.user.photoURL)
+        })
+        .catch((err) => { console.log(err) })
+}
+export function signOutGoogle(e) {
+    e.preventDefault()
+    auth.signOut()   
+        .then(function () {
+        localStorage.clear()
+       console.log('Signout Succesfull')
+    }, function(error) {
+       console.log('Signout Failed')  
+    });
+ }
+
 export function requestPermission() {
     console.log('Requesting permission...');
     Notification.requestPermission().then((permission) => {
         if (permission === 'granted') {
             console.log('Notification permission granted.');
             const messaging = getMessaging();
-            getToken(messaging, {vapidKey:process.env.REACT_APP_VAPID_KEY}).then((currentToken) => {
+            getToken(messaging, { vapidKey: process.env.REACT_APP_VAPID_KEY }).then((currentToken) => {
                 if (currentToken) {
                     console.log(currentToken)
                     fetch('http://localhost:5000/usrtkn', {
@@ -18,7 +51,7 @@ export function requestPermission() {
                             token: currentToken,
                             userid: 'test',
                         })
-                    }).then(res => res.json() ).then(data => { console.log(data) })
+                    }).then(res => res.json()).then(data => { console.log(data) })
                 } else {
                     // Show permission request UI
                     console.log('No registration token available. Request permission to generate one.');
@@ -30,16 +63,4 @@ export function requestPermission() {
             });
         }
     })
-}
-export const fcminit = () => {
-    initializeApp({
-        apiKey: "AIzaSyC7-ZMp-ywmGmTMI1jjRrQDTZaspiI1Rrk",
-        authDomain: "farm-life-d1c77.firebaseapp.com",
-        projectId: "farm-life-d1c77",
-        storageBucket: "farm-life-d1c77.appspot.com",
-        messagingSenderId: "51901782398",
-        appId: "1:51901782398:web:4e476c39b6018404a7fb2f",
-        measurementId: "G-TP7ZGR6B26"
-    })
-
 }
